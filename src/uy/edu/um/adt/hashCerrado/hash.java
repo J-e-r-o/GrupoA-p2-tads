b23d397a93a599f2da4cad4dc37ff6e4cc87dbe4
Jeronimo <deleonjeronimo079@gmail.com>
2024-05-20 10:58:44 -0300

    Poniendo TestUnitarios en src

--- src/uy/edu/um/adt/hashCerrado/hash.java
+++ src/uy/edu/um/adt/hashCerrado/hash.java
@@ -1,5 +1,12 @@
 package uy.edu.um.adt.hashCerrado;
 
-public class hash implements hashInterfaze{
+public class hash<K,V> implements hashInterfaze<K,V>{
     
+    public void put(K key, V value){}
+    
+    public boolean contains(K key){
+        return true;
+    }
+    
+    public void remove(K clave){}
 }
