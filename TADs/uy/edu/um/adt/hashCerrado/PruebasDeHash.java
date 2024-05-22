c49890f69b2d41961ed63897d24424e4b30ad008
Jeronimo <deleonjeronimo079@gmail.com>
2024-05-22 18:31:27 -0300

    Hash cerrado hecho (Falta la funcion Conteains )

--- TADs/uy/edu/um/adt/hashCerrado/PruebasDeHash.java
+++ TADs/uy/edu/um/adt/hashCerrado/PruebasDeHash.java
@@ -3,22 +3,23 @@ package uy.edu.um.adt.hashCerrado;
 public class PruebasDeHash {
     //The code computes a polynomial function of 37 by use of Horner’s rule.    
 
-    public static int hash( String key, int tableSize )
-    {
-     int hashVal = 0;
-    
-     for( int i = 0; i < key.length( ); i++ )
-     hashVal = 37 * hashVal + key.charAt( i );
-    
-     hashVal %= tableSize;
-     if( hashVal < 0 )
-     hashVal += tableSize;
-    
-     return hashVal;
-    }
 
     public static void main(String[] args) {
-        System.out.println( hash( "Holaaa", 1007 ));
+
+    hash<Integer,String> h= new hash<>();
+    h.put(5,"Hola" );
+    h.put(6,"qer");
+    h.put(55, "Otra");
+    h.put(8,"Saltador");
+    h.put(10,"Algo");
+    System.out.println(h.ShowHash());
+    h.put(4,"EEEEEEEEE");    
+    h.put(7, "SQL");
+    h.put(81,"H");    
+    h.put(33,"Jero");
+
+    
+    System.out.println(h.ShowHash());    
     }
 
 
--- TADs/uy/edu/um/adt/hashCerrado/hash.java
+++ TADs/uy/edu/um/adt/hashCerrado/hash.java
@@ -1,5 +1,4 @@
 package uy.edu.um.adt.hashCerrado;
-import java.util.ArrayList;
 import java.util.Vector;
 
 
@@ -10,12 +9,17 @@ public class hash<K,V> implements hashInterfaze<K,V>{
     
     Vector<HashNode<K, V>> Hash;
     int tableSize;
-    
+    int capacity;
+    int percentage_capacity;
+ 
+
     public hash(){
+        capacity=0;
         tableSize=11;
-        // Ya se crea un vector con 11 espacios nulos
-        Hash= new Vector<HashNode<K,V>>(tableSize);
-       
+        percentage_capacity=0;
+        Hash= new Vector<>(tableSize);
+        for (int i = 0; i < tableSize; i++) {
+            Hash.add(null);}
     }   
     
     
@@ -24,28 +28,90 @@ public class hash<K,V> implements hashInterfaze<K,V>{
         HashNode<K,V> nuevoHashNode= new HashNode<K,V>(key, value);
         int LugarDelHash=HashFunction(key);
         
+        
         //Tenemos que ver que el Lugar insertado este vacio
-        while(Hash.get(LugarDelHash) !=null){
-            LugarDelHash+=1;
+         while(Hash.get(LugarDelHash) !=null){
+             if (LugarDelHash>= tableSize){
+                LugarDelHash=0;
+            }
+                LugarDelHash+=1;
         }
-        
-        Hash.get(LugarDelHash) = nuevoHashNode;
-        
-        
+        Hash.set(LugarDelHash,nuevoHashNode);
+        this.capacity+=1;
+        this.percentage_capacity=((capacity*100)/tableSize);
+
+        //Hacer rehashing cuando el hash es muy grande 
+        if(percentage_capacity>=75){
+            this.Rehash();
+        }
     }
     
     public boolean contains(K key){
-        return true;
+        //Esta mal
+        return Hash.contains(key);    
     }
     
-    public void remove(K clave){}
+    public void remove(K clave){
+        int indiceDelBorrado= HashFunction(clave);
+        System.out.println();
+    
+        while (indiceDelBorrado<this.tableSize){
+            if (this.Hash.get(indiceDelBorrado).key==clave){
+                this.Hash.get(indiceDelBorrado).borrado=true;
+                break;
+            }    
+                indiceDelBorrado+=1;
+            }
+    }
 
-    private void Rehash(){}
+    private void Rehash(){
+        NextPrimo np = new NextPrimo();
+        int tablesizeVIEJO=this.tableSize;
+        
+        this.tableSize= np.nextPrimo(tableSize*2);
+        Vector<HashNode<K,V>> Hash_nuevo= new Vector<>(tableSize);
+        
+        //LLena todo el Hash de nodos nulos
+        for(int i = 0; i < tableSize; i++){
+            Hash_nuevo.add(null);
+        }
+        for (int i = 0; i < tablesizeVIEJO; i++) {
+            //Caso 1 y caso 2 no hacemos nada
+            if(Hash.get(i)==null){
+                
+            }    
+            if(Hash.get(i)!=null){
+                if(Hash.get(i).borrado==true){   
+                }
+                else{
+                    HashNode<K,V> Nodo_Que_Tenemos_Que_Mudar=Hash.get(i);
+                    int lugarEnNuevoHash =HashFunction(Nodo_Que_Tenemos_Que_Mudar.key);
+                    Hash_nuevo.set(lugarEnNuevoHash, Nodo_Que_Tenemos_Que_Mudar);
+                }
+            }
+        }
 
-    public int HashFunction(K key){
+        this.Hash=Hash_nuevo;
+    }
+
+    private int HashFunction(K key){
         return (key.hashCode()) % tableSize;
     }
 
+    public String ShowHash(){
+        String resultado="{";
+        for(int i=0;i<tableSize;i++){
+            if (this.Hash.get(i)==null || this.Hash.get(i).borrado==true){
+                resultado = resultado+ "NULL, ";
+            }
+            else{
+            resultado= resultado + this.Hash.get(i).valor+", ";
+        }
+    }
+        resultado= resultado +"\b" +"\b" +"}";
+        return resultado;
+    }
 
+    
     
 }
